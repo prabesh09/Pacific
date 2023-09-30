3c46d3184723e5ef96f378eb0418fa278165cbd2
Prabesh Shrestha <124156720+prabesh09@users.noreply.github.com>
2023-09-30 08:27:36 +0545

    Update timer.js

--- timer.js
+++ timer.js
@@ -3,19 +3,20 @@ const content = document.getElementById("container");
 
 const updateTime = () => {
     const time = new Date();
-    let hour = time.getHours().toString().padStart(2, "0");
-    let status = true;
-    if (hour > 12) {
-        hour -= 12;
-        hour = hour.toString().padStart(2, "0");
-        status = false;
-    } else if (hour == 0) {
-        hour = 12;
-        hour = hour.toString().padStart(2, "0");
+    let hour = time.getHours();
+    let status = "AM";
+
+    if (hour >= 12) {
+        status = "PM";
+        if (hour > 12) {
+            hour -= 12;
+        }
     }
+
+    hour = hour.toString().padStart(2, "0");
     const minute = time.getMinutes().toString().padStart(2, "0");
     const second = time.getSeconds().toString().padStart(2, "0");
-    const displayTime = `${hour}:${minute}:${second} ${status ? "AM" : "PM"}`;
+    const displayTime = `${hour}:${minute}:${second} ${status}`;
     content.innerHTML = displayTime;
 };
 
